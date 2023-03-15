5df987a9cf2a6c6ab7b746faf5fe56d2e76ddfe8
wRidho788 <wridho246@gmail.com>
2023-03-15 17:17:44 +0700

    fix

--- src/js/routes/extras/Menu.js
+++ src/js/routes/extras/Menu.js
@@ -5,7 +5,6 @@ const Menu = (
   <GroupMenuItem title="Extras" icon="windows" to="/extras">
     <MenuItem title="Pricing Tables" to="/extras/pricing-tables" />
     <MenuItem title="Typography" to="/extras/typograpy" />
-    <MenuItem title="Glyphicons" to="/extras/glyphicons" />
     <MenuItem title="Icons" to="/extras/icons" />
     <MenuItem title="Media Gallery" to="/extras/media-gallery" />
   </GroupMenuItem>
